98377e5a7a67bf8c0148bc923bfac266cdaef8fd
shaf_haider007 <shaf.shafheidar@gmail.com>
2025-01-31 01:36:30 +0500

    production ready

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -10,7 +12,13 @@ app.get("/about",(req,res)=>{
     res.send("About Page");
     }
 );
-app.listen(port, () => {
+
+app.get("/contact",(req,res)=>{
+    res.send("<input type='text' placeholder='Enter your name'><button>Submit</button>");
+});
+
+
+app.listen(process.env.PORT, () => {
     console.log(`Example app listening at http://localhost:${port}`);
     }
 );
